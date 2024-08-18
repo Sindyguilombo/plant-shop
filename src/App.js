d43bfdaf44d7a13e1caca70e87b8d1a1f7060dd1
Sindyguilombo <s.guilombo1@lanuevaamerica.edu.co>
2024-08-18 06:15:36 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+
+import React from 'react';
+import './App.css'; // Asegúrate de que este archivo exista o elimina la línea si no es necesario
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a la Tienda de Plantas</h1>
+        <p>Edita y guarda para volver a cargar.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/redux/reducers.js
@@ -0,0 +1,12 @@
+// src/redux/reducers.js
+
+import { combineReducers } from 'redux';
+import cartReducer from './cartReducer'; 
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+ 
+});
+
+export default rootReducer;
+
